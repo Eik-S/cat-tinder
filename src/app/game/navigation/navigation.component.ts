f1741157ccb300fce7ce0a62b4e35d7828300221
Eike Mücksch <eikem@posteo.de>
2020-07-23 12:22:30 +0200

    Navigation Component: Disable clicks while transitioning

--- src/app/game/navigation/navigation.component.ts
+++ src/app/game/navigation/navigation.component.ts
@@ -11,6 +11,7 @@ export class NavigationComponent implements OnInit {
   @Output() voted = new EventEmitter<string>();
 
   gameData: GameData;
+  transitioning: boolean;
 
   constructor( private activityService: ActivityService) {
     this.activityService.gameData.subscribe( gameData => {
@@ -21,17 +22,33 @@ export class NavigationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // blocks clicks if the last image is still transitioning
+  // (prevents spamming)
+  startTransitionTimeout() {
+    this.transitioning = true;
+    setTimeout(() => this.transitioning = false, 200);
+  }
+
   likeTheCat(): void {
+    if (this.transitioning) return;
+    this.startTransitionTimeout();
+
     this.gameData.likes += 1;
     this.voted.emit('like');
   }
 
   dislikeTheCat(): void {
+    if (this.transitioning) return;
+    this.startTransitionTimeout();
+
     this.gameData.dislikes += 1;
     this.voted.emit('dislike')
   }
 
   skipTheCat(): void {
+    if (this.transitioning) return;
+    this.startTransitionTimeout();
+
     this.gameData.skipped += 1;
     this.voted.emit('skip')
   }
